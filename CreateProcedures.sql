1bde736665f0e4c637a4f1db8880c1a6b31e5530
Chris Tomlin <59611371+tomlinc98@users.noreply.github.com>
2022-04-30 12:51:01 +0100

    Update CreateProcedures.sql

--- CreateProcedures.sql
+++ CreateProcedures.sql
@@ -1,19 +1,10 @@
-CREATE OR REPLACE PROCEDURE addAdmin(
-		name in ADMIN.adminName%TYPE,
-		address in ADMIN.adminAddress%TYPE,
-		phone in ADMIN.adminPhone%TYPE,
-		password in ADMIN.adminPassword%TYPE
-		)
-		IS
-BEGIN
-	INSERT INTO ADMIN (adminId,adminName,adminAddress,adminPhone,adminPassword) VALUES (seqAddAdmin.nextval,name,address,phone,password);	
-	Commit;
-END addAdmin;
-/
-
-    CREATE TABLE category (
-        id                             NUMBER NOT NULL,
-        cat_name                       VARCHAR2(30),
-        CONSTRAINT category_id_pk 
-            PRIMARY KEY(id)
-);
+CREATE PROCEDURE calculateRent(vehicleId IN NUMBER, rentAmount OUT NUMBER) AS
+  -- variable to hold the number of days
+  rentDuration NUMBER;
+  BEGIN
+    -- fetch dates, calculate the difference in days and assign it to a variable
+    SELECT (return_date - rent_date) into rentDuration FROM vehiclerent
+    WHERE vehicle_id = vehicleId;
+    -- calculate the rent amount assuming the daily rate to be 50 and assign it to output parameter
+    rentAmount := rentDuration * 50;
+  END;
